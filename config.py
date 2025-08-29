2fdae31b8f317222e715c0a62cad09c5b20b0acd
Nari <narimanethr@gmail.com>
2025-08-29 16:20:57 +0200

    Ajout de beauty_analytics.db, JupyterCommandes.ipynb, config.py et import_season_data_simple.py depuis la branche dev

---
+++ config.py
@@ -0,0 +1,8 @@
+# Configuration de la base de données PostgreSQL
+DB_CONFIG = {
+    'host': 'localhost',
+    'port': 5432,
+    'dbname': 'beauty_analytics',
+    'user': 'postgres',  # Remplacez par votre nom d'utilisateur PostgreSQL si différent
+    'password': 'hello1234'  # Mot de passe corrigé
+}
